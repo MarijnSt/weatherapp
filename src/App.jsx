2afe45767274edafd4d0fe074445a5ebae183031
MarijnSt <marijn.stammeleer@gmail.com>
2019-06-06 22:32:40 +0200

    top en bottom components toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 
-import './src/style.css'
+import './assets/style.css'
+
+//sections importeren
+import TopSection from './components/top.jsx';
+import BottomSection from './components/bottom.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <div className="main-container">Main Container</div>
+        <div className="main-container">
+          <div className="top">
+            <TopSection />
+          </div>
+          <div className="bottom">
+            <BottomSection />
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/components/top.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class TopSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  //lege render return geeft een container terug
+  render() {
+    return <div>TOP SECTION</div>
+  }
+}
